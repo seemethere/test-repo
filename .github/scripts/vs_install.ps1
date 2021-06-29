f301415fcf6f52993475a40d09ad4e0cd33801c4
Jane Xu <janeyx@fb.com>
2021-06-29 17:43:30 -0400

    -p

--- .github/scripts/vs_install.ps1
+++ .github/scripts/vs_install.ps1
@@ -4,7 +4,7 @@
 # 16.8.5 BuildTools
 $VS_DOWNLOAD_LINK = "https://download.visualstudio.microsoft.com/download/pr/20130c62-1bc8-43d6-b4f0-c20bb7c79113/145a319d79a83376915d8f855605e152ef5f6fa2b2f1d2dca411fb03722eea72/vs_BuildTools.exe"
 $COLLECT_DOWNLOAD_LINK = "https://aka.ms/vscollect.exe"
-$VS_INSTALL_ARGS = @("--nocache","--wait", "--add Microsoft.VisualStudio.Workload.VCTools",
+$VS_INSTALL_ARGS = @("--nocache", "-p", "--add Microsoft.VisualStudio.Workload.VCTools",
                                                      "--add Microsoft.Component.MSBuild",
                                                      "--add Microsoft.VisualStudio.Component.Roslyn.Compiler",
                                                      "--add Microsoft.VisualStudio.Component.TextTemplating",
@@ -28,7 +28,7 @@ if (Test-Path "${env:ProgramFiles(x86)}\Microsoft Visual Studio\Installer\vswher
     $existingPath = & "${env:ProgramFiles(x86)}\Microsoft Visual Studio\Installer\vswhere.exe" -products "Microsoft.VisualStudio.Product.BuildTools" -version "[16, 17)" -property installationPath
     if ($existingPath -ne $null) {
         echo "Found existing BuildTools installation in $existingPath"
-        $VS_UNINSTALL_ARGS = @("uninstall", "--installPath", "`"$existingPath`"","--wait")
+        $VS_UNINSTALL_ARGS = @("uninstall", "--installPath", "-p", "`"$existingPath`"")
         $process = Start-Process "${PWD}\vs_installer.exe" -ArgumentList $VS_UNINSTALL_ARGS -NoNewWindow -Wait -PassThru
         $exitCode = $process.ExitCode
         if (($exitCode -ne 0) -and ($exitCode -ne 3010)) {
