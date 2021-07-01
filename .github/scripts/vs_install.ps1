b2a4c236e0782966390ba45c3637f8b13ff43241
Jane Xu <janeyx@fb.com>
2021-07-01 14:34:08 -0400

    change it back to 16.8.5

--- .github/scripts/vs_install.ps1
+++ .github/scripts/vs_install.ps1
@@ -1,8 +1,8 @@
 # https://developercommunity.visualstudio.com/t/install-specific-version-of-vs-component/1142479
 # Where to find the links: https://docs.microsoft.com/en-us/visualstudio/releases/2019/history#release-dates-and-build-numbers
 
-# 16.8.6 BuildTools
-$VS_DOWNLOAD_LINK = "https://download.visualstudio.microsoft.com/download/pr/8aaeb3c2-46bb-4444-9ca6-0361b60b2d16/1f4552565c70ee4355fcb4bf561b11dc23ae0a20cb6b4bcd0546d6819e545eae/vs_BuildTools.exe"
+# 16.8.5 BuildTools
+$VS_DOWNLOAD_LINK = "https://download.visualstudio.microsoft.com/download/pr/20130c62-1bc8-43d6-b4f0-c20bb7c79113/145a319d79a83376915d8f855605e152ef5f6fa2b2f1d2dca411fb03722eea72/vs_BuildTools.exe"
 $COLLECT_DOWNLOAD_LINK = "https://aka.ms/vscollect.exe"
 $VS_INSTALL_ARGS = @("--nocache","--wait", "-p", "--add Microsoft.VisualStudio.Workload.VCTools",
                                                      "--add Microsoft.Component.MSBuild",
@@ -21,31 +21,26 @@ if (${env:INSTALL_WINDOWS_SDK} -eq "1") {
 if (Test-Path "${env:ProgramFiles(x86)}\Microsoft Visual Studio\Installer\vswhere.exe") {
     $existingPath = & "${env:ProgramFiles(x86)}\Microsoft Visual Studio\Installer\vswhere.exe" -products "Microsoft.VisualStudio.Product.BuildTools" -version "[16, 17)" -property installationPath
     if ($existingPath -ne $null) {
-        echo "Found existing BuildTools installation in $existingPath"
-        echo "Removing all VS installers and installations"
-        & "${env:ProgramFiles(x86)}\Microsoft Visual Studio\Installer\resources\app\layout\InstallCleanup.exe" -i
+        echo "Found existing BuildTools installation in $existingPath. Removing all VS installers and installations."
         $process = Start-Process "${env:ProgramFiles(x86)}\Microsoft Visual Studio\Installer\resources\app\layout\InstallCleanup.exe" -ArgumentList @("-i") -NoNewWindow -Wait -PassThru
-        # $VS_UNINSTALL_ARGS = @("uninstall", "--installPath", "-p", "`"$existingPath`"","--wait")
-        # $process = Start-Process "${PWD}\vs_installer.exe" -ArgumentList $VS_UNINSTALL_ARGS -NoNewWindow -Wait -PassThru
         $exitCode = $process.ExitCode
         if (($exitCode -ne 0) -and ($exitCode -ne 3010)) {
-            echo "Original BuildTools uninstall failed with code $exitCode"
+            echo "Installation cleanup failed with code $exitCode"
             exit 1
         }
-        echo "Original BuildTools uninstalled"
+        echo "Visual Studio successfully uninstalled"
     }
 }
 
 curl.exe --retry 3 -kL $VS_DOWNLOAD_LINK --output vs_installer.exe
 if ($LASTEXITCODE -ne 0) {
-    echo "Download of the VS 2019 Version 16.8.6 installer failed"
+    echo "Download of the VS 2019 Version 16.8.5 installer failed"
     exit 1
 }
 
 $process = Start-Process "${PWD}\vs_installer.exe" -ArgumentList $VS_INSTALL_ARGS -NoNewWindow -Wait -PassThru
 Remove-Item -Path vs_installer.exe -Force
 $exitCode = $process.ExitCode
-echo $exitCode
 if (($exitCode -ne 0) -and ($exitCode -ne 3010)) {
     echo "VS 2019 installer exited with code $exitCode, which should be one of [0, 3010]."
     curl.exe --retry 3 -kL $COLLECT_DOWNLOAD_LINK --output Collect.exe
@@ -55,6 +50,7 @@ if (($exitCode -ne 0) -and ($exitCode -ne 3010)) {
     }
     Start-Process "${PWD}\Collect.exe" -NoNewWindow -Wait -PassThru
     New-Item -Path "C:\w\build-results" -ItemType "directory" -Force
-    Copy-Item -Path "${PWD}\AppData\Local\Temp\vslogs.zip" -Destination "C:\w\build-results\"
+    Copy-Item -Path "${env:TEMP}\vslogs.zip" -Destination "C:\w\build-results\"
     exit 1
 }
+echo "Installation complete."
